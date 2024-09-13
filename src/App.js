5d60b7969f7c1580039a80a1a4ae968cab2d51c6
rachelcheework <150230042+rachelcheework@users.noreply.github.com>
2024-09-13 20:40:53 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <TopNav/> 
       <div className='mt-0'> {/* mt-5 if nav bar is fixed='top' */}
         <Routes>
-          <Route path="/" element={<Home/>}/>
+          <Route exact path="/" element={<Home/>}/>
           <Route path='/about' element={<About/>}/>
           {/* <Route path='/projects' element={<Projects/>}/> */}
           <Route path='/contact' element={<Contact/>}/>
